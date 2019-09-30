69a0fefdccd45af2292625c379aefe5914581108
Guillaume Chau <alphadelta.fg@gmail.com>
2019-10-01 00:20:18 +0200

    feat: package|project-type basic resolvers

---
+++ packages/awesomejs-backend/src/schema/package.ts
@@ -0,0 +1,54 @@
+import gql from 'graphql-tag'
+import { IResolvers } from 'graphql-tools'
+import { Context } from '@/context'
+import { query as q } from 'faunadb'
+
+export const typeDefs = gql`
+type Package {
+  id: ID!
+  name: String!
+  projectType: ProjectType!
+  author: PackageAuthor!
+  description: String
+  stars: Int
+}
+
+type PackageAuthor {
+  id: ID!
+  name: String!
+}
+
+extend type Query {
+  package (id: ID!): Package
+}
+`
+
+export const resolvers: IResolvers<any, Context> = {
+  Package: {
+    author: async (pkg, args, ctx) => {
+      const { ref: { id }, data } = await ctx.db.query(
+        q.Get(q.Ref(q.Collection('PackageAuthors'), pkg.authorId))
+      )
+      if (data) {
+        return {
+          id,
+          ...data,
+        }
+      }
+    }
+  },
+
+  Query: {
+    package: async (root, { id }, ctx) => {
+      const { data } = await ctx.db.query(
+        q.Get(q.Ref(q.Collection('Packages'), id))
+      )
+      if (data) {
+        return {
+          id,
+          ...data,
+        }
+      }
+    }
+  }
+}
---
+++ packages/awesomejs-backend/src/schema/project-type.ts
@@ -0,0 +1,66 @@
+import gql from 'graphql-tag'
+import { IResolvers } from 'graphql-tools'
+import { Context } from '@/context'
+import { query as q, values } from 'faunadb'
+
+export const typeDefs = gql`
+type ProjectType {
+  id: ID!
+  name: String!
+  logo: String!
+  packages: [Package!]!
+}
+
+extend type Query {
+  projectTypes: [ProjectType!]!
+  projectType (id: ID!): ProjectType
+}
+`
+
+export const resolvers: IResolvers<any, Context> = {
+  ProjectType: {
+    packages: async (projectType, args, ctx) => {
+      const { data } = await ctx.db.query(
+        q.Map(
+          q.Paginate(
+            q.Match(q.Index('packages_projecttypeid'), projectType.id)
+          ),
+          q.Lambda('X', q.Get(q.Var('X')))
+        )
+      )
+      return data.map((doc: values.Document) => ({
+        id: doc.ref.id,
+        ...doc.data,
+      }))
+    }
+  },
+
+  Query: {
+    projectTypes: async (root, args, ctx) => {
+      const { data } = await ctx.db.query(
+        q.Map(
+          q.Paginate(
+            q.Match(q.Index('all_projecttypes'))
+          ),
+          q.Lambda('X', q.Get(q.Var('X')))
+        )
+      )
+      return data.map((doc: values.Document) => ({
+        id: doc.ref.id,
+        ...doc.data,
+      }))
+    },
+
+    projectType: async (root, { id }, ctx) => {
+      const { data } = await ctx.db.query(
+        q.Get(q.Ref(q.Collection('ProjectTypes'), id))
+      )
+      if (data) {
+        return {
+          id,
+          ...data
+        }
+      }
+    }
+  }
+}
