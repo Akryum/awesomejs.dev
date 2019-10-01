f6b93a4d257390a15ea316cac3270e3e9685a8b6
Guillaume Chau <alphadelta.fg@gmail.com>
2019-10-01 20:01:30 +0200

    feat: format stars count

--- packages/awesomejs-frontend/src/components/pkg/PackageStars.vue
+++ packages/awesomejs-frontend/src/components/pkg/PackageStars.vue
@@ -1,18 +1,28 @@
 <template>
   <div class="flex text-purple-500">
     <div class="mr-1">
-      {{ count }}
+      {{ formatted }}
     </div>
     <i class="material-icons">star</i>
   </div>
 </template>
 
 <script>
+import millify from 'millify'
+
 export default {
   props: {
     count: {
       type: Number,
       required: true,
+    },
+  },
+
+  computed: {
+    formatted () {
+      return millify(this.count, {
+        precision: 1,
+      })
     },
   },
 }
