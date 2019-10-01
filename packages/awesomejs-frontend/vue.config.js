a81f4ec0f879165c6e33a971263a10029542de04
Guillaume Chau <alphadelta.fg@gmail.com>
2019-10-01 12:40:44 +0200

    feat: setup pwa manifest

---
+++ packages/awesomejs-frontend/vue.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  pwa: {
+    name: 'AwesomeJS',
+    themeColor: '#F7DF1D',
+    msTileColor: '#25241F',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+    iconPaths: {
+      favicon32: 'img/icons/guijs-32.png',
+      favicon16: 'img/icons/guijs-16.png',
+      appleTouchIcon: 'img/icons/guijs-152.png',
+      maskIcon: 'img/icons/guijs-safari-mask.svg',
+      msTileImage: 'img/icons/guijs-144.png',
+    },
+  },
+}
