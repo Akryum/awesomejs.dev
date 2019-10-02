2664dc28555379390629bb0e61e9c747c179fafa
Guillaume Chau <alphadelta.fg@gmail.com>
2019-10-02 05:46:41 +0200

    fix(purge css): whitelist <a>

--- packages/frontend/postcss.config.js
+++ packages/frontend/postcss.config.js
@@ -10,7 +10,10 @@ module.exports = {
         const contentWithoutStyleBlocks = content.replace(/<style[^]+?<\/style>/gi, '')
         return contentWithoutStyleBlocks.match(/[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g) || []
       },
-      whitelist: [],
+      whitelist: [
+        // <router-link/>
+        'a',
+      ],
       whitelistPatterns: [ /-(leave|enter|appear)(|-(to|from|active))$/, /^(?!(|.*?:)cursor-move).+-move$/, /^router-link(|-exact)-active$/ ],
     }),
     require('autoprefixer')(),
