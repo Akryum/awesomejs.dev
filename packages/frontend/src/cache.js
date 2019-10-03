f48ffe1c65ff170869789741be2d6d15fc247df7
Guillaume Chau <alphadelta.fg@gmail.com>
2019-10-03 13:11:22 +0200

    fix: cache readQuery crash

--- packages/frontend/src/cache.js
+++ packages/frontend/src/cache.js
@@ -6,24 +6,28 @@ export const cache = new InMemoryCache({
   cacheRedirects: {
     Query: {
       projectType: (_, { slug }) => {
-        // If we already have loaded the list of project types
-        // we want to redirect to the corresponding project type
-        // instead of doing an unnecessary request to the API
-        const { projectTypes } = cache.readQuery({
-          query: gql`query ReachProjectTypesCache {
-            projectTypes {
-              id
-              slug
+        try {
+          // If we already have loaded the list of project types
+          // we want to redirect to the corresponding project type
+          // instead of doing an unnecessary request to the API
+          const { projectTypes } = cache.readQuery({
+            query: gql`query ReachProjectTypesCache {
+              projectTypes {
+                id
+                slug
+              }
+            }`,
+          })
+          // Lookup for the project type with corresponding slug
+          if (projectTypes) {
+            const p = projectTypes.find(t => t.slug === slug)
+            if (p) {
+              // Cache redirect
+              return toIdValue(cache.config.dataIdFromObject({ __typename: 'ProjectType', id: p.id }))
             }
-          }`,
-        })
-        // Lookup for the project type with corresponding slug
-        if (projectTypes) {
-          const p = projectTypes.find(t => t.slug === slug)
-          if (p) {
-            // Cache redirect
-            return toIdValue(cache.config.dataIdFromObject({ __typename: 'ProjectType', id: p.id }))
           }
+        } catch (e) {
+          // https://github.com/apollographql/apollo-client/issues/1542
         }
       },
 
