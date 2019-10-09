5e67dc7a45ea383eba2cab84bc3215fb6ef9d959
Guillaume Chau <alphadelta.fg@gmail.com>
2019-10-09 19:27:16 +0200

    chore: upgrade to @vue/cli 4.x

--- packages/frontend/vue.config.js
+++ packages/frontend/vue.config.js
@@ -12,5 +12,35 @@ module.exports = {
       maskIcon: 'img/icons/guijs-safari-mask.svg',
       msTileImage: 'img/icons/guijs-144.png',
     },
+    manifestOptions: {
+      'background_color': '#25241F',
+      'icons': [
+        {
+          'src': 'img/icons/guijs-48.png',
+          'sizes': '48x48',
+          'type': 'image/png',
+        }, {
+          'src': 'img/icons/guijs-72.png',
+          'sizes': '72x72',
+          'type': 'image/png',
+        }, {
+          'src': 'img/icons/guijs-96.png',
+          'sizes': '96x96',
+          'type': 'image/png',
+        }, {
+          'src': 'img/icons/guijs-144.png',
+          'sizes': '144x144',
+          'type': 'image/png',
+        }, {
+          'src': 'img/icons/guijs-168.png',
+          'sizes': '168x168',
+          'type': 'image/png',
+        }, {
+          'src': 'img/icons/guijs-192.png',
+          'sizes': '192x192',
+          'type': 'image/png',
+        },
+      ],
+    },
   },
 }
