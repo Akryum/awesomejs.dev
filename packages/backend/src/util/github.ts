9d7826a5f280d6add7b8bed3ea3ca20e941cb939
Guillaume Chau <guillaume.b.chau@gmail.com>
2020-01-01 11:01:39 +0100

    fix: release count split error

--- packages/backend/src/util/github.ts
+++ packages/backend/src/util/github.ts
@@ -1,6 +1,7 @@
 import Octokit from '@octokit/rest'
 
 export function getPageTotalCount<T> (result: Octokit.Response<T>) {
+  if (!result.headers.link) { return 0 }
   const links = result.headers.link.split(',').map(
     (text) => text.split(';').map((part) => part.trim()),
   )
