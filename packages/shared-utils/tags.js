809a68782295b229beddf1a8a4e6aced93af889c
Guillaume Chau <guillaume.b.chau@gmail.com>
2020-01-04 04:21:18 +0100

    fix: use commonjs modules

--- packages/shared-utils/tags.js
+++ packages/shared-utils/tags.js
@@ -1,4 +1,7 @@
-export function isSpecialTag(tag) {
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function isSpecialTag(tag) {
     return tag === 'official' ||
         tag.startsWith('version:');
 }
+exports.isSpecialTag = isSpecialTag;
