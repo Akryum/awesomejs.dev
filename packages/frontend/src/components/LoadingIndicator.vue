3d8e82deaa566d2bb87fef45444d9b1467ec3e7b
Guillaume Chau <guillaume.b.chau@gmail.com>
2020-01-05 02:07:29 +0100

    feat: delay loading animation

--- packages/frontend/src/components/LoadingIndicator.vue
+++ packages/frontend/src/components/LoadingIndicator.vue
@@ -8,17 +8,24 @@
 
 <style lang="postcss" scoped>
 .animation {
-  animation: loading 2s infinite ease-in-out;
+  animation: fade-in .5s .3s backwards, loading 2s infinite ease-in-out;
+}
+
+@keyframes fade-in {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
 }
 
 @keyframes loading {
   0%, 100% {
-    transform: scale(.8);
-    opacity: .7;
+    transform: scale(.7);
   }
   50% {
     transform: scale(1);
-    opacity: 1;
   }
 }
 </style>
